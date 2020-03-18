4938efb6893aed7beaf35d260d8b35354c451dc0
Affect3d <32643159+Affect3d@users.noreply.github.com>
2020-03-18 15:18:02 +0100

    Update JavaApplication85.java

--- JavaApplication85.java
+++ JavaApplication85.java
@@ -3,48 +3,65 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaapplication85;
+package wzorce_proj;
+
 /**
  *
- * @author Student
+ * @author kuber
  */
-public class JavaApplication85 {
-    
-    public interface IMyList<T>{
+public class Main {
+
+    public interface IMyList<T> {
+
         public void add(T element);
+
         public void add(int index, T element);
+
         public void clear();
+
         public T pop();
+
         public T get(int index);
+
         public int size();
+
         T remove(int index);
     }
-    
+
     public class MyCustomList<T> implements IMyList<T> {
+
         private T[] tablica;
-        
+
+//        public MyCustomList()
+//        {
+//            T[] tablica;
+//        }
         @Override
-        public T remove(int index) {return null;}
+        public T remove(int index) {
+            return null;
+        }
 
         @Override
         public void add(T element) {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            tablica[tablica.length] = element;
         }
 
         @Override
         public void add(int index, T element) {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            tablica[index] = element;
         }
 
         @Override
         public void clear() {
-            T[] tab = (T[])new Object[0];
+            T[] tab = (T[]) new Object[0];
             tablica = tab;
         }
 
         @Override
         public T pop() {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            T popped = tablica[tablica.length - 1];
+            tablica[tablica.length - 1] = null;
+            return popped;
         }
 
         @Override
@@ -57,11 +74,11 @@ public class JavaApplication85 {
             return tablica.length;
         }
     }
-    
-    
-    public static void main(String[] args) {
-    System.out.println("DrugiProgram");
-    
+
+    public void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Help I've Mc'fallen");
+        MyCustomList<> tablica = new MyCustomList<>();
     }
-    
+
 }
